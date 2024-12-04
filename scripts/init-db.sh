9516644290e42a16ed111d129a46b4b8feed3a99
richardhapb <richard.penab@gmail.com>
2024-12-03 22:02:50 -0300

    feat: first tools setup

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -2,19 +2,17 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE TABLE ip_analisis (
+    CREATE SCHEMA IF NOT EXISTS ${POSTGRES_SCHEMA};
+    CREATE TABLE IF NOT EXISTS ${POSTGRES_SCHEMA}.ip_analisis (
         ip varchar(15) PRIMARY KEY,
         pais varchar(30),
         reportes_totales integer,
-        fecha_ult_reporte datetime,
+        fecha_ult_reporte timestamp,  
         estado varchar(15)
     );
-    CREATE TABLE comentarios (
-        id INTEGER PRIMARY KEY,
+    CREATE TABLE IF NOT EXISTS ${POSTGRES_SCHEMA}.comentarios (
+        id SERIAL PRIMARY KEY,
         comentario TEXT,
-        ip varchar(15) references ip_analisis(ip)
+        ip varchar(15) REFERENCES ${POSTGRES_SCHEMA}.ip_analisis(ip)
     );
 EOSQL
-
-
-
